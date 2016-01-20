379a34563e7d4cad09f1afc111827ebe6a225593
Vidura Mudalige <vbmudalige@gmail.com>
2016-01-20 23:27:29 +0530

    Incorporated average number of dengue cases to calculate the dengue mobility with neighbors. Also fixed the Viterbi exception problem my introducing maximumDengueMobilityValueOfTheTrainingSequence variable

--- HMM/src/com/vidura/fyp/hmm/HMModel.java
+++ HMM/src/com/vidura/fyp/hmm/HMModel.java
@@ -15,12 +15,13 @@ public class HMModel {
 	public static void main(String[] args) throws MatlabConnectionException, MatlabInvocationException {
 
 		String mohName = "Kaduwela";
-		int noOfWeeksToTrain = 35; // maximum number of training weeks is 51
-		int sizeOfDengueInterval = 5;
-		int sizeOfMobilityInterval = 2;
+		int noOfWeeksToTrain = 40; // maximum number of training weeks is 50 since we can use only 51 weeks and at least one week is needed for testing
+		int sizeOfDengueInterval = 10;
+		int sizeOfMobilityInterval = 20;
 		int maxNoOfDengueCases = 200;
-		int maxMobilityValue = 200;
-		Double mobilityThreshold = 0.1;
+		int maxMobilityValue = 20000;
+		int maximumDengueMobilityValueOfTheTrainingSequence = 200;
+		Double mobilityThreshold = 1.0;
 
 		String hiddenStateSeq = "";
 		int mohId = -1;
@@ -35,6 +36,7 @@ public class HMModel {
 		List<ArrayList<Integer>> testDengueMobility = new ArrayList<ArrayList<Integer>>();
 		HashMap<Integer, Integer> dengueLevelDiscretizar = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> dengueMobilityDiscretizar = new HashMap<Integer, Integer>();
+		HashMap<Integer, Double> finalNormalizedMobilityWithNeighbors = new HashMap<Integer, Double>();
 
 		MatlabProxyFactory factory = new MatlabProxyFactory();
 		MatlabProxy proxy = factory.getProxy();
@@ -52,6 +54,9 @@ public class HMModel {
 		// Initialize dengueMobilityDiscretizar
 		for (int i = 0, j = 1; i < maxMobilityValue; i++) {
 			if (i != 0 && i % sizeOfMobilityInterval == 0) {
+				//removing unknown maximum states
+				if (i > maximumDengueMobilityValueOfTheTrainingSequence)
+					j--;
 				j++;
 			}
 			dengueMobilityDiscretizar.put(i, j);
@@ -98,11 +103,24 @@ public class HMModel {
 			while (population.readRecord()) {
 				normalizedMobilityWithNeighbors.add(mobilityWithNeighbors.get(i) / Integer.parseInt(population.get(2)));
 				i++;
-			}
-
+			}			
 			// Get dengue cases to calculate dengue mobility sequences
 			while (dengueCases.readRecord()) {
 				int recordMohId = Integer.parseInt(dengueCases.get(0));
+				
+				
+				
+				
+				// TODO adding number of total dengue cases to calculate dengue mobility
+				int sumOfDengueCases = 0;
+				for (int j = 3; j < noOfWeeksToTrain + 3; j++) {
+					sumOfDengueCases += Integer.parseInt(dengueCases.get(j));
+				}
+				finalNormalizedMobilityWithNeighbors.put(recordMohId, (normalizedMobilityWithNeighbors.get(recordMohId) * sumOfDengueCases)/noOfWeeksToTrain);
+				
+				
+				
+				
 				if (recordMohId == mohId) {
 					// j = 3 since the lag should be 1 week
 					for (int j = 3; j < noOfWeeksToTrain + 3; j++) {
@@ -114,14 +132,20 @@ public class HMModel {
 					}
 					continue;
 				}
-				double recordMohMobility = normalizedMobilityWithNeighbors.get(recordMohId);
+
+				double recordMohMobility = finalNormalizedMobilityWithNeighbors.get(recordMohId);
+				//System.out.println("************************    " + recordMohMobility + "        " + sumOfDengueCases);
 				if (recordMohMobility > mobilityThreshold) {
+					
+					//System.out.println("************************    " + recordMohMobility + "        " + sumOfDengueCases);
 					ArrayList<Integer> recordMohDengueMobility = new ArrayList<Integer>();
 					ArrayList<Integer> recordTestMohDengueMobility = new ArrayList<Integer>();
 					for (int j = 2; j < noOfWeeksToTrain + 2; j++) {
 						Integer dengueMobilityIntValue = (int) (Integer.parseInt(dengueCases.get(j))
 								* recordMohMobility);
+						//System.out.println("*****************   " + dengueMobilityIntValue + "     " + Integer.parseInt(dengueCases.get(j))	* recordMohMobility);
 						recordMohDengueMobility.add(dengueMobilityDiscretizar.get(dengueMobilityIntValue));
+						//System.out.println("*****************    " + dengueMobilityDiscretizar.get(dengueMobilityIntValue) + "      " + dengueMobilityIntValue);
 					}
 					for (int j1 = noOfWeeksToTrain + 2; j1 < 54; j1++) {
 						Integer testDengueMobilityIntValue = (int) (Integer.parseInt(dengueCases.get(j1))
@@ -144,14 +168,19 @@ public class HMModel {
 			dengueCases.close();
 		}
 
-		int noOfTests = 49 - noOfWeeksToTrain;
+		int noOfTests = 51 - noOfWeeksToTrain;
+
+		System.out.println("figure(\"" + mohName + "\"); clear();");
+		System.out.println("series(\"Predicted\", \"ko:\");");
 
 		for (int k = 1; k < noOfTests; k++) {
 			int counter = 0;
 			float sumOfWeights = 0;
 			float normalizedSumOfPredictions = 0;
+			//for each neighbor, 
 			for (ArrayList<Integer> array : dengueMobility) {
 				String emissionSeq = "";
+				//consider the dengue mobility value of each week
 				for (int val : array) {
 					emissionSeq += Integer.toString(val) + " ";
 				}
@@ -162,6 +191,7 @@ public class HMModel {
 				int prediction = hmm.getPrediction(proxy, processor, hiddenStateSeq, emissionSeq, thisWeek);
 				int prevPrediction = hmm.getPrediction(proxy, processor, hiddenStateSeq, emissionSeq, lastWeek);
 
+				// used k - 1 instead of k since hidden states start from week 2
 				int actualValue = hiddenStateTestSeq.get(k - 1);
 				int error = prevPrediction - actualValue;
 				float weight = 1;
@@ -177,9 +207,16 @@ public class HMModel {
 			}
 
 			int finalPrediction = (int) (normalizedSumOfPredictions / sumOfWeights);
-			System.out.println(finalPrediction + "    " + hiddenStateTestSeq.get(k));
+			//System.out.println(finalPrediction + "    " + hiddenStateTestSeq.get(k));
+			System.out.print("plotPoint(" + Integer.toString(k - 1) + ", " + finalPrediction + ");");
 		}
-
+		
+		
+		System.out.println("\nseries(\"Actual\", \"m+\");");
+		//i1 = 1 since we cannot predict the first week
+		for (int i1 = 1; i1 < noOfTests; i1++) {
+			System.out.print("plotPoint(" + Integer.toString(i1 - 1) + ", " + hiddenStateTestSeq.get(i1) + ");");
+		}
 		proxy.disconnect();
 	}
 
